ac3e4b7ce0ef394675824446a17160f6b88e9686
Max <m.k4rp0v@yandex.ru>
2022-11-13 04:28:47 +0300

    upd 1

--- database.py
+++ database.py
@@ -1,16 +1,17 @@
+import sqlite3
 import sqlite3 as sl
+from datetime import datetime
+from tabulate import tabulate
 
 
-def connect_database():
+def connect_database() -> sqlite3.Connection:
     connect = sl.connect('DataBase.db')
-    data = connect.execute("select count(*) from sqlite_master where type='table' and name='goods'")
+    data = connect.execute("select count(*) from sqlite_master where type='table' and name='students'")
     for row in data:
-        # если таких таблиц нет
         if row[0] == 0:
-            # создаём таблицу для товаров
             with connect:
                 connect.execute("""
-                CREATE TABLE main_table (
+                CREATE TABLE students (
                 id INTEGER PRIMARY KEY,
                 last_name VARCHAR(50),
                 first_name VARCHAR(50),
@@ -20,14 +21,68 @@ def connect_database():
                 exam_date VARCHAR(10)
                 );
                 """)
+    return connect
 
 
 class Student:
 
-    def _init__(self, last_name, first_name, second_name, value, exam_date):
+    def __init__(self, last_name: str, first_name: str, second_name: str, value: int, exam_date: datetime):
         self.last_name = last_name
         self.first_name = first_name
         self.second_name = second_name
         self.value = value
-        self.exam_date = exam_date
+        self.exam_date = datetime.strftime(exam_date, '%d.%m.%Y')
+
+    def is_student_in_database(self) -> list:
+        connect = connect_database()
+        sql = 'SELECT * FROM students WHERE last_name=? and first_name=? and  second_name=?'
+        data = (
+             self.last_name,
+             self.first_name,
+             self.second_name
+             )
+        with connect:
+            return connect.execute(sql, data).fetchall()
+
+    def add_to_database(self) -> str:
+        connect = connect_database()
+        if self.is_student_in_database():
+            return 'Ошибка: Данные уже записаны в БД'
+        sql = 'INSERT INTO students (last_name, first_name, second_name, value, paid, exam_date) values (?, ?, ?, ?, ?, ?)'
+        data = (
+             self.last_name,
+             self.first_name,
+             self.second_name,
+             self.value,
+             0,
+             self.exam_date)
+        with connect:
+            connect.execute(sql, data)
+        return 'Успех: Данные записаны в БД'
+
+
+def get_database(key: str) -> str:
+    sorting_keys = {
+        'По фамилии': 0,
+        'По дате': 5,
+        'По сумме': 3,
+        'По оплате': 4,
+        'По дате добавления': 1,
+    }
+    connect = connect_database()
+    sql = 'SELECT * FROM students'
+    database_entries = connect.execute(sql).fetchall()
+    database_list = []
+    for entry in sorted(database_entries, key=lambda x: x[sorting_keys[key]]):
+        entry = list(entry)
+        entry[5] = 'Оплачен' if entry[5] == 1 else 'Не оплачен'
+        data_for_table = [
+            f"{entry[1]} {entry[2]} {entry[3]}",
+            entry[4],
+            entry[5],
+            entry[6]
+        ]
+        database_list.append(data_for_table)
+    database_table = tabulate(database_list, headers=['ФИО', 'Сумма', 'Оплачен', 'Дата'])
+    return database_table
 
--- runbot.py
+++ runbot.py
@@ -1,31 +1,56 @@
+from datetime import datetime
+
 import telebot
 from telebot import types
 
 from database import Student
+from database import get_database
 
 
-with open('auth.txt', 'r') as file:
+with open('bot_auth.txt', 'r') as file:
     bot_token = file.read()
 bot = telebot.TeleBot(bot_token)
 
-# Keyboards
+user_auth = []
+with open('user_auth.txt', 'r') as file:
+    user_auth.append(file.read())
+
+# Клавиатуры
+# Главное меню
 main_menu_markup = types.ReplyKeyboardMarkup(row_width=2)
-main_menu_button_add = types.KeyboardButton('Добавить')
-main_menu_button_change_name = types.KeyboardButton('Изменить ФИО')
-main_menu_button_change_date = types.KeyboardButton('Изменить дату')
+main_menu_button_add = types.KeyboardButton('Как добавить')
+main_menu_button_change_name = types.KeyboardButton('Как изменить ФИО')
+main_menu_button_change_date = types.KeyboardButton('Как изменить дату')
 main_menu_button_show_database = types.KeyboardButton('Список')
-main_menu_button_pay = types.KeyboardButton('Оплатить')
+main_menu_button_payment = types.KeyboardButton('Как изменить "Оплачен"')
 main_menu_markup.add(
     main_menu_button_add,
     main_menu_button_change_name,
     main_menu_button_change_date,
+    main_menu_button_payment,
     main_menu_button_show_database,
-    main_menu_button_pay
 )
 
+# Меню "Назад"
 back_menu_markup = types.ReplyKeyboardMarkup(row_width=1)
 back_menu_button = types.KeyboardButton('Назад в меню')
 back_menu_markup.add(
+    back_menu_button
+)
+
+# Меню выбора фильтра БД
+filter_menu_markup = types.ReplyKeyboardMarkup(row_width=2)
+filter_by_last_name_button = types.KeyboardButton('По фамилии')
+filter_by_date_button = types.KeyboardButton('По дате')
+filter_by_payment_button = types.KeyboardButton('По оплате')
+filter_by_value_button = types.KeyboardButton('По сумме')
+filter_by_id_button = types.KeyboardButton('По порядку добавления')
+filter_menu_markup.add(
+    filter_by_last_name_button,
+    filter_by_date_button,
+    filter_by_payment_button,
+    filter_by_value_button,
+    filter_by_id_button,
     back_menu_button
 )
 
@@ -33,29 +58,58 @@ back_menu_markup.add(
 @bot.message_handler(func=lambda message: message.text == 'Назад в меню')
 @bot.message_handler(commands=['start'])
 def authorization(message):
-    bot.send_message(message.chat.id, 'Выбери пункт меню:', reply_markup=main_menu_markup)
+    bot.send_message(message.chat.id, 'Выбери один из пунктов меню:', reply_markup=main_menu_markup)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Добавить')
+@bot.message_handler(func=lambda message: message.text == 'Как добавить')
+def add_button(message):
+    bot.send_message(message.chat.id, 'Напиши ФИО, сумму и дату через запятую.\n'
+                                      'Пример: Иванов Иван Иванович, 100, 22.10.2022')
+
+
+def change_date_button(message):
+    pass
+
+
+def change_payment_button(message):
+    pass
+
+
+@bot.message_handler(func=lambda message: message.text == 'Список')
+def print_database_button(message):
+    bot.send_message(message.chat.id, 'Выбери способ фильтрации списка', reply_markup=filter_menu_markup)
+
+
+@bot.message_handler(func=lambda message: message.text in ['По фамилии', 'По дате', 'По оплате', 'По сумме', 'По порядку добавления'])
+def print_database(message):
+    bot.send_message(message.chat.id, get_database(message.text))
+
+
+@bot.message_handler(content_types=['text'])
 def add_student(message):
-    student_info_message = bot.send_message(message.chat.id, 'Напиши ФИО, сумму и дату экзамена через запятую.'
-                                                   '\n Пример: Иванов Иван Иванович, 100, 22.10.2022',
-                                  reply_markup=back_menu_markup)
-    bot.register_next_step_handler(student_info_message, get_person)
-
-
-def get_person(message):
-    student_info = message.text.split(',')
-    student_fio = student_info[0].split(' ')
-    print(student_fio)
-    if len(student_fio != 3):
-        student_info_message = bot.send_message(message.chat.id, 'ФИО должно состоять из трех слов! Напиши всю информацию заново')
-        bot.register_next_step_handler(student_info_message, get_person)
-
-    for i in student_info:
-        bot.send_message(message.chat.id, i)
-
-
+    user_message = message.text.split(',')
+    if len(user_message) != 3:
+        bot.send_message(message.chat.id, 'В сообщении недостаточно данных для записи в БД')
+        return
+    else:
+        fio = user_message[0].split(' ')
+        value = user_message[1].strip(' ')
+    if len(fio) != 3:
+        bot.send_message(message.chat.id, 'В ФИО недостаточно слов')
+        return
+    if not value.isdigit():
+        bot.send_message(message.chat.id, 'Сумма должна содержать только цифры')
+        return
+    else:
+        value = int(user_message[1])
+    try:
+        date = datetime.strptime(user_message[2].strip(' '), '%d.%m.%Y')
+    except ValueError:
+        bot.send_message(message.chat.id, 'Дата должна быть написана в формате dd.mm.yyyy')
+        return
+    new_student = Student(fio[0], fio[1], fio[2], value, date)
+    database_entry = new_student.add_to_database()
+    bot.send_message(message.chat.id, database_entry)
 
 
 bot.infinity_polling()
